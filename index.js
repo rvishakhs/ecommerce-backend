26f75dc22dbdbae427c7d125981b0ccfb54ca7b5
rvishakhs <rvishakhs@gmail.com>
2023-03-23 20:47:18 +0530

    mongodb installed

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express() // assigning express into a variable
 const dotenv = require('dotenv').config() // to enable dotenv we have to call dotenv module in node
 const PORT = process.env.PORT || 4000 // assiging port
 
-
+app.use("/", (req, res) => {
+    res.send("hello world")
+})
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
