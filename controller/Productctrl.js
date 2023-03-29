e7e6ee32ee01db8d043d776a2e6cf83f45ef9493
rvishakhs <rvishakhs@gmail.com>
2023-03-29 23:44:07 +0530

    getall products and finding one products are done

--- controller/Productctrl.js
+++ controller/Productctrl.js
@@ -1,14 +1,41 @@
 const Product = require("../modals/productModal")
 const asynchandler = require("express-async-handler")
 
+// Create a new product
+
 const createProduct = asynchandler(async(req, res) => {
     try {
         const newproduct = await Product.create(req.body)
         res.json(newproduct)
     } catch (err) {
-        throw new Error(err.Message)
-    }
-    
+        throw new Error(`This error is related to creating product, and  details are ${err.Message}`)
+    }   
 })
 
-module.exports = {createProduct}
+// Find one Product 
+
+const getproduct = asynchandler(async (req, res) => {
+    const {id} = req.params
+    
+    try{
+        const findproduct = await Product.findById(id)
+        res.json(findproduct)
+    } catch (err) {
+        throw new Error(`This error is related to Find a product, and  details are ${err.Message}`)
+    }
+
+})
+
+// Find all Product
+
+const getAllProducts = asynchandler(async (req, res) => {
+    try {
+        const allproducts = await Product.find()
+        res.json(allproducts)
+    } catch (err){
+        throw new Error(`This error is related to Finding all products, and  details are ${err.Message}`)
+    }
+})
+
+
+module.exports = {createProduct, getproduct, getAllProducts}
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createProduct } = require('../controller/Productctrl');
+const { createProduct, getproduct, getAllProducts } = require('../controller/Productctrl');
 const router = express.Router();
 
 router.post("/", createProduct)
+router.get("/:id", getproduct)
+router.get("/", getAllProducts)
 
 
 module.exports = router
