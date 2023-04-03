b483033137d918b4c1aaac22a721d072f4daf537
rvishakhs <rvishakhs@gmail.com>
2023-04-03 13:40:09 +0530

    Emailsending functionality for forget password

---
+++ controller/emailcontroler.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+const asynchandler = require("asynchandler");
+
+const sentemail = asynchandler(async(data, req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_ID, // generated ethereal user
+      pass: process.env.MAIL_PASS, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Eshoppers " <accounts@eshoppers.com>', // sender address
+    to: data.to, // list of receivers
+    subject: data.subject, // Subject line
+    text: data.text, // plain text body
+    html: data.html, // html body
+  });
+})
+
+module.exports = sentemail
