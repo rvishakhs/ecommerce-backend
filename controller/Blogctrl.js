03ec9e4e7cf777cb2d152c74f70ac07ed0fbd3c1
rvishakhs <rvishakhs@gmail.com>
2023-04-03 20:51:35 +0530

    Blogs create update and get a blog updated

--- controller/Blogctrl.js
+++ controller/Blogctrl.js
@@ -3,17 +3,50 @@ const User = require("../modals/userModal")
 const asynchandler = require("express-async-handler")
 
 
+
+// Create a new blog
 const createBlog = asynchandler(async (req, res) => {
     try{
-        const blog = await Blog.create(req.body)
+        const newBlog = await Blog.create(req.body)
         res.json({
             status : 200,
-            blog
+            newBlog
         })
     }catch (err) {
         throw new Error(`This error is related to creating blog post for more details : ${err.message}`)
     }
-
 })
 
-module.exports = {createBlog}
+// Update Existing Blog
+
+const updateBlog = asynchandler(async (req, res) => {
+    const {id} = req.params;
+    try{
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body, {
+            new : true,
+        })
+        res.json(updateBlog)
+    } catch (err) {
+        throw new Error(`This error is related to updating blog post for more details : ${err.message}`)
+    }
+})
+
+// For viewing one Blog
+
+const viewSingleBlog = asynchandler(async (req, res) => {
+    const {id} = req.params;
+    try{
+        const ViewBlog = await Blog.findById(id)
+        const updateViews = await Blog.findByIdAndUpdate(id, {
+            $inc : {numOfViews : 1},
+        }, {
+            new : true,
+        })
+        res.json(updateViews)
+    } catch (err) {
+        throw new Error(`This error is related to updating blog post for more details : ${err.message}`)
+    }
+    
+})
+
+module.exports = {createBlog, updateBlog, viewSingleBlog}
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const  {createBlog}  = require('../controller/Blogctrl')
+const  {createBlog, updateBlog, viewSingleBlog}  = require('../controller/Blogctrl')
 const { authHandler, isAdmin } = require('../middleware/AuthVerification')
 
 const router = express.Router()
 
-router.post("/", createBlog)
+router.post("/createblog", authHandler, isAdmin, createBlog)
+router.put("/updateblog/:id", authHandler, isAdmin, updateBlog)
+router.get("/:id", viewSingleBlog)
 
 module.exports = router
